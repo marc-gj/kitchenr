5db6e6066d7d1325d91a6e75d1354f0b57782178
marc-gj <marc-gj@live.com>
2018-03-21 13:26:08 -0400

    Format indent

--- config/passport.js
+++ config/passport.js
@@ -4,20 +4,20 @@ const User = require('../models/user');
 const config = require('../config/database');
 
 module.exports = function(passport){
-	let opts = {};
-	opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
-	opts.secretOrKey = config.secret;
-	passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-		User.getUserById(jwt_payload._doc._id, (err, user) =>{
-			//console.log(jwt_payload);
-			if (err) {
-				return done(err, false);
-			}
-			if (user) {
-				return done(null, user);
-			} else {
-				return done(null, false);
-			}
-		});
-	}));
+  let opts = {};
+  opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+  opts.secretOrKey = config.secret;
+  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    User.getUserById(jwt_payload._doc._id, (err, user) =>{
+      //console.log(jwt_payload);
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    });
+  }));
 };
